884b07ed8a0a57fb40a01d84561266f4ebbb4d4c
Juanjo <juanjomp85gmail.com>
2023-12-18 12:21:42 +0100

    Seccion 5 - AddCharacter

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent { }
