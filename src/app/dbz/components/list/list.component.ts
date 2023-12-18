f2c5b1eac3c66bae50c4d054efc437125f6fe1a8
Juanjo <juanjomp85gmail.com>
2023-12-18 22:00:23 +0100

    Seccion 5 - ngClass

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,9 +1,18 @@
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()//Valor por defecto
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power:10
+  }]
+
+ }
